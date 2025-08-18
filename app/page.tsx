d562631303b523ad3162c221d02f94e62e0e850c
Atimscreative <timilehinsunday144@gmail.com>
2025-08-18 01:00:40 +0100

    Add navigation links to Home page and create vercel.json for rewrites

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
       <h1 className="text-6xl font-bold text-iq-100">Team IQ</h1>
+      <Link href="/login">Login</Link>
+      <Link href="/organization">Organizatin</Link>
     </div>
   );
 }
