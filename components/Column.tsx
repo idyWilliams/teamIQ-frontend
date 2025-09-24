3d627691017296c1a41c9f24af337569bed26c76
Sifan23 <sifanyg123@gmail.com>
2025-09-24 19:39:29 +0300

    feat: updated thet column title section

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,27 +1,40 @@
-
-import { useDroppable } from '@dnd-kit/core';
-import { TaskCard } from './TaskCard';
-import { Column as ColumnType, Task } from './types';
+import { useDroppable } from "@dnd-kit/core";
+import { TaskCard } from "./TaskCard";
+import { Column as ColumnType, Task } from "./types";
+import { Avatar } from "@radix-ui/react-avatar";
+import  DragOutline  from "@/components/icons/DragOutline";
 
 type ColumnProps = {
   column: ColumnType;
   tasks: Task[];
 };
 
-export function Column({ column, tasks }: ColumnProps) {
 
-    const { setNodeRef } = useDroppable({
+export function Column({ column, tasks }: ColumnProps) {
+  const { setNodeRef } = useDroppable({
     id: column.id,
   });
 
   return (
-    <div className="flex w-80 flex-col rounded-lg bg-blue-800 p-4">
-      <h2 className="mb-4 font-semibold text-neutral-100">{column.title}</h2>
-      <div ref={setNodeRef} className="flex flex-1 flex-col gap-4">
+    <div className="flex w-150  flex-col rounded-lg ">
+      <div className="flex flex-row justify-between items-center ">
+        <h2 className="mb-4 font-semibold text-[#0E0E0E]">{column.title}</h2>
+        <Avatar
+          className={`w-8 h-8 flex items-center justify-center rounded-full ${column.bgColor}`}
+        >
+          <p className={`font-extrabold text-[11px] ${column.textColor}`}>
+            {column.taskCount}
+          </p>
+        </Avatar>
+      </div>
+      <div className="flex items-center mb-4 gap-2">
+        <DragOutline size="20" />
+        <p className="text-sm font-normal text-[#626262]">{column.subTitle}</p>
+      </div>
+
+      <div ref={setNodeRef} className="flex flex-1 flex-col gap-2">
         {tasks.map((task) => {
-         return (
-            <TaskCard key={task.id} task={task} />
-         )
+          return <TaskCard key={task.id} task={task} />;
         })}
       </div>
     </div>
