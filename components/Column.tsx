c07670ec0987cb003daa0068c69a0ec2ba112365
Sifan23 <sifanyg123@gmail.com>
2025-10-01 12:09:49 +0300

    feat: reamoved the add dialogue

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,20 +1,10 @@
 "use client";
-import { useState } from "react";
+
 import { useDroppable } from "@dnd-kit/core";
 import { TaskCard } from "./TaskCard";
 import { Column as ColumnType, Task } from "./types";
 import { Avatar } from "@radix-ui/react-avatar";
 import DragOutline from "@/components/icons/DragOutline";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import { TaskForm } from "./TaskForm";
-import { Plus } from "lucide-react";
 
 type ColumnProps = {
   column: ColumnType;
@@ -22,14 +12,13 @@ type ColumnProps = {
 };
 
 export function Column({ column, tasks }: ColumnProps) {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const { setNodeRef } = useDroppable({
     id: column.id,
   });
 
   return (
-    <div className="flex w-150 flex-col rounded-lg">
+    <div className="flex flex-col rounded-lg p-4 h-full">
   
       <div className="flex flex-row justify-between items-center">
         <h2 className="mb-4 font-semibold text-[#0E0E0E]">{column.title}</h2>
@@ -48,31 +37,10 @@ export function Column({ column, tasks }: ColumnProps) {
         <p className="text-sm font-normal text-[#626262]">{column.subTitle}</p>
       </div>
 
-      {/* Task cards */}
       <div ref={setNodeRef} className="flex flex-1 flex-col gap-2">
         {tasks.map((task) => (
           <TaskCard key={task.id} task={task} />
         ))}
-      </div>
-
-      
-      <div className="mt-4">
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogTrigger asChild>
-            <Button variant="outline" className="w-full">
-               <Plus className="w-5 h-5" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Add a Task to {column.title}</DialogTitle>
-            </DialogHeader>
-            <TaskForm
-              defaultStatus={column.id}
-              onClose={() => setIsDialogOpen(false)}
-            />
-          </DialogContent>
-        </Dialog>
       </div>
     </div>
   );
