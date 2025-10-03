3b34820cc9b48e513cc6edcacb11efcf76a7082f
Mattia Lavai <mattialavai@gmail.com>
2025-10-03 14:50:45 +0000

    Setup services folder, axios instance, and API endpoint mapping

--- services/hooks/useUsers.ts
+++ services/hooks/useUsers.ts
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import api from "@/lib/axios";
+import api from "@/services/axios";
 
 export function useUsers() {
   return useQuery({
