0fd20954477f88c12672985fd0482e64cded505a
Sifan23 <sifanyg123@gmail.com>
2025-10-08 11:28:09 +0300

    feat: constanst for dashboard

---
+++ constants/active-blockers.ts
@@ -0,0 +1,56 @@
+export const activeBlockers = [
+  {
+    id: 1,
+    name: "Alex",
+    blockedOn: "Blocked on API dependencies",
+    time: "2hrs",
+  },
+  {
+    id: 2,
+    name: "Sarah",
+    blockedOn: "Waiting for design approval",
+    time: "4hrs",
+  },
+  {
+    id: 3,
+    name: "Mike",
+    blockedOn: "Infrastructure deployment issue",
+    time: "1hr",
+  },
+  { 
+    id: 4,
+    name: "Alex",
+    blockedOn: "Blocked on API dependencies",
+    time: "2hrs",
+  },
+  {
+    id: 5,
+    name: "Sarah",
+    blockedOn: "Waiting for design approval",
+    time: "4hrs",
+  },
+  {
+    id: 6,
+    name: "Mike",
+    blockedOn: "Infrastructure deployment issue",
+    time: "1hr",
+  },
+  {
+    id: 7,
+    name: "Alex",
+    blockedOn: "Blocked on API dependencies",
+    time: "2hrs",
+  },
+  {
+    id: 8,
+    name: "Sarah",
+    blockedOn: "Waiting for design approval",
+    time: "4hrs",
+  },
+  {
+    id: 9,
+    name: "Mike",
+    blockedOn: "Infrastructure deployment issue",
+    time: "1hr",
+  },
+];
