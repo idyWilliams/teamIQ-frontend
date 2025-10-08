f2f80dbb956ea08630c9108afdaceba1a7089f03
Kolade Faith <faithkolade0507@gmail.com>
2025-10-08 21:05:31 +0100

    Mapped through thedata

--- app/(admin)/organization/page.tsx
+++ app/(admin)/organization/page.tsx
@@ -52,6 +52,7 @@ export default function OverviewPage() {
           <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
       </div>
+      <ProjectStatus />
       <div className=" w-[300px] h-full border-l"></div>
     </>
   );
--- components/project-status.tsx
+++ components/project-status.tsx
@@ -1,9 +1,36 @@
 import { Dot } from "lucide-react";
 
+const Datas = [
+  {
+    name: "Mobile App v2.0...",
+    status: "In Progress",
+    description: "Next: Beta",
+    Date: "Release - Jun 25",
+  },
+  {
+    name: "API Modernization...",
+    status: "In Progress",
+    description: "Next: Security",
+    Date: "Review - Jul 20",
+  },
+  {
+    name: "API Modernization...",
+    status: "In Progress",
+    description: "Next: Security",
+    Date: "Review - Jul 20",
+  },
+  {
+    name: "Customer Portal...",
+    status: "In Progress",
+    description: "Next: Phase 2",
+    Date: "Start - Jul 30",
+  },
+];
+
 const ProjectStatus = () => {
   return (
     <>
-      <div className="mx-4">
+      <div className="mx-4 w-full">
         <div className="flex justify-between mb-3">
           <h2 className="text-[#0E0E0E] font-semibold text-1">
             Project Status Summary
@@ -11,59 +38,25 @@ const ProjectStatus = () => {
           <h2 className="text-[#0E0E0E] font-semibold text-1">
             View Project List
           </h2>
-        </div>{" "}
+        </div>
+
         <div className="flex gap-4 flex-col">
-          {" "}
-          <div>
-            <div className="border-l-[#086ACE] border-l-1 border bg-muted/50 rounded-lg px-5 py-3">
+          {Datas.map((data, index) => (
+            <div
+              key={index}
+              className="border-l-[#086ACE] border-l-1 border bg-muted/50 rounded-lg px-5 py-3">
               <div className="flex justify-between mb-1 items-center">
                 <h3 className="text-[14px] text-[#141414] font-semibold">
-                  Mobile App v2.0...
+                  {data.name}
                 </h3>
                 <div className="text-[#8A8CD9] text-[12px] flex items-center justify-center">
-                  <Dot className="text-[12px]" /> <span>In Progress</span>
+                  <Dot className="text-[12px]" /> <span>{data.status}</span>
                 </div>
               </div>
-              <p className="text-[#626262] text-[12px]">Next: Beta </p>
-              <p className="text-[#626262] text-[12px]">Release - Jun 25</p>
+              <p className="text-[#626262] text-[12px]">{data.description}</p>
+              <p className="text-[#626262] text-[12px]">{data.Date}</p>
             </div>
-          </div>
-          <div className="border-l-[#086ACE] border-l-1 border bg-muted/50 rounded-lg px-5 py-3">
-            <div className="flex justify-between mb-1 items-center">
-              <h3 className="text-[14px] text-[#141414] font-semibold">
-                Mobile App v2.0...
-              </h3>
-              <div className="text-[#8A8CD9] text-[12px] flex items-center justify-center">
-                <Dot className="text-[12px]" /> <span>In Progress</span>
-              </div>
-            </div>
-            <p className="text-[#626262] text-[12px]">Next: Beta </p>
-            <p className="text-[#626262] text-[12px]">Release - Jun 25</p>
-          </div>
-          <div className="border-l-[#086ACE] border-l-1 border bg-muted/50 rounded-lg px-5 py-3">
-            <div className="flex justify-between mb-1 items-center">
-              <h3 className="text-[14px] text-[#141414] font-semibold">
-                Mobile App v2.0...
-              </h3>
-              <div className="text-[#8A8CD9] text-[12px] flex items-center justify-center">
-                <Dot className="text-[12px]" /> <span>In Progress</span>
-              </div>
-            </div>
-            <p className="text-[#626262] text-[12px]">Next: Beta </p>
-            <p className="text-[#626262] text-[12px]">Release - Jun 25</p>
-          </div>
-          <div className="border-l-[#086ACE] border-l-1 border bg-muted/50 rounded-lg px-5 py-3">
-            <div className="flex justify-between mb-1 items-center">
-              <h3 className="text-[14px] text-[#141414] font-semibold">
-                Mobile App v2.0...
-              </h3>
-              <div className="text-[#8A8CD9] text-[12px] flex items-center justify-center">
-                <Dot className="text-[12px]" /> <span>In Progress</span>
-              </div>
-            </div>
-            <p className="text-[#626262] text-[12px]">Next: Beta </p>
-            <p className="text-[#626262] text-[12px]">Release - Jun 25</p>
-          </div>
+          ))}
         </div>
       </div>
     </>
