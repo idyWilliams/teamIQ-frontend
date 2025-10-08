a7ad30447230d7a78f551b338ca3f00bb9c10c7e
Sifan23 <sifanyg123@gmail.com>
2025-10-08 13:18:33 +0300

    feat: active blockers constants

--- constants/active-blockers.ts
+++ constants/active-blockers.ts
@@ -35,22 +35,4 @@ export const activeBlockers = [
     blockedOn: "Infrastructure deployment issue",
     time: "1hr",
   },
-  {
-    id: 7,
-    name: "Alex",
-    blockedOn: "Blocked on API dependencies",
-    time: "2hrs",
-  },
-  {
-    id: 8,
-    name: "Sarah",
-    blockedOn: "Waiting for design approval",
-    time: "4hrs",
-  },
-  {
-    id: 9,
-    name: "Mike",
-    blockedOn: "Infrastructure deployment issue",
-    time: "1hr",
-  },
 ];
