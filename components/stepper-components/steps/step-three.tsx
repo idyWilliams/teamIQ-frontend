a5f70ee70497ae9bfb3134e66f5cee273f5bdaae
Sifan23 <sifanyg123@gmail.com>
2025-11-03 11:46:57 +0300

    feat: step one, two and three components of the stepper

---
+++ components/stepper-components/steps/step-three.tsx
@@ -0,0 +1,50 @@
+'use client';
+import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../../ui/accordion';
+import DownArrow from '../../icons/DownArrow';
+import RightArrow from '@/components/icons/RightArrow';
+import VersionControlSetup from '../stepper-forms/version-control-setup';
+
+interface StepThreeProps {
+  next: () => void;
+}
+
+const StepThree = ({ next }: StepThreeProps) => {
+  return (
+    <div className="w-full">
+      <Accordion
+        type="single"
+        collapsible
+        defaultValue="user-permission"
+        className="w-full"
+      >
+        <AccordionItem value="user-permission">
+          <AccordionTrigger className="group flex cursor-pointer items-center gap-2 hover:no-underline [&_.lucide-chevron-down]:hidden [&>svg]:!rotate-0">
+            <RightArrow
+              size="20"
+              className="block group-data-[state=open]:hidden"
+            />
+            <DownArrow
+              size="20"
+              className="hidden group-data-[state=open]:block"
+            />
+            <p className="flex-1 text-xl font-semibold">
+              User & Permission Sync
+            </p>
+          </AccordionTrigger>
+
+          <AccordionContent>
+            <VersionControlSetup onSubmit={next} hideButton={false} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default StepThree;
