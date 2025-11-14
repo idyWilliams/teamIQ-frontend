9088905dc4e856d8e726d4101c3faad1deeefd6a
Sifan23 <sifanyg123@gmail.com>
2025-11-14 15:28:43 +0300

    feat: mapping real intern name on header/appbar

--- components/user-dashboard-component/Header.tsx
+++ components/user-dashboard-component/Header.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Input } from '../ui/input';
 import { Search, Bell } from 'lucide-react';
+import { useAuthStore } from '@/store/useAuthStore';
 
 type HeaderProps = {
   isMobile?: boolean;
@@ -10,6 +11,7 @@ type HeaderProps = {
 };
 
 const Header = ({ isMobile, onOpenNotification }: HeaderProps) => {
+  const {user} = useAuthStore()
   return (
     <header className="flex h-[10%] items-center justify-end gap-4 p-2 md:justify-between">
       {/* Mobile View */}
@@ -33,9 +35,9 @@ const Header = ({ isMobile, onOpenNotification }: HeaderProps) => {
           {/* Left - Initials + Full Name */}
           <div className="inline-flex items-center gap-2">
             <span className="flex size-10 items-center justify-center rounded-full bg-[#FFECE5] font-bold text-neutral-800">
-              JA
+              {user.first_name[0]}{user.last_name[0]}
             </span>
-            <span className="font-bold text-neutral-800">James Alfred</span>
+            <span className="font-bold text-neutral-800">{user.first_name}{" "}{user.last_name}</span>
           </div>
 
           {/* Right - Search Input + Bell */}
