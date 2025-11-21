d584564fdefa8047a2a35ecf31d77d3da8bb4948
user.name <adeolagodsonomomowo@gmail.com>
2025-11-21 15:24:22 +0100

    fix: improve myskill mobile responsiveness

--- components/skill-rate.tsx
+++ components/skill-rate.tsx
@@ -11,17 +11,15 @@ const SkillRate = ({
     <div className="relative flex h-[40px] items-center justify-end overflow-clip rounded-[8px] bg-[#F7F7F7] px-[24px] lg:h-[49px]">
       <div
         className={
-          'absolute top-0 bottom-0 left-[-1px] flex -translate-x-[2%] -skew-x-[20deg] items-center justify-center overflow-clip rounded-tr-md rounded-br-md font-bold text-white lg:text-[14px]'
+          'absolute top-0 bottom-0 left-[-1px] flex -translate-x-[2%]  -skew-x-[20deg] items-center justify-center overflow-clip rounded-tr-md rounded-br-md font-bold text-white lg:text-[14px]'
         }
         style={{
-          // clipPath: `polygon(0 0, 100% 0, ${
-          //   skillLevel < 100 ? 90 : 100
-          // }% 100%, 0 100%)`,
           width: `${skillLevel + 5}%`,
           backgroundColor: `${color}`,
         }}
       >
         {skillLevel}%
+        
       </div>
       <span className="relative z-10 text-[13px] font-semibold lg:text-[14px]">
         {skill}
